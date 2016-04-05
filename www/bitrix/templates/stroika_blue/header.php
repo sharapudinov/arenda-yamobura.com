357560e2aeeca965c774ddadb7f5d779ca1cd887
svoyn <svoynrav@yandex.ru>
2016-04-06 00:49:36 +0300

    Небольшие правки

--- www/bitrix/templates/stroika_blue/header.php
+++ www/bitrix/templates/stroika_blue/header.php
@@ -17,6 +17,28 @@ AddMessage2Log($APPLICATION->GetCurDir());
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/favicon-128.png" sizes="128x128" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="<?=SITE_TEMPLATE_PATH;?>/css/themes/images/favicon/mstile-310x310.png" />
+
 	<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH;?>/javascript/jquery-1.10.2.js" type="text/javascript"></script>
 	<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH;?>/javascript/jquery-2.1.1.min.js" type="text/javascript"></script>
 	<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH;?>/javascript/jquery.bxslider.js"></script>
