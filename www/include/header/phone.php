a544203f2f46c33773a5419e2c40d3e298ef4dbf
svoyn <svoynrav@yandex.ru>
2016-06-21 12:03:11 +0300

    Телефон в хидере

--- www/include/header/phone.php
+++ www/include/header/phone.php
@@ -1,2 +1,2 @@
-<a href="tel:79037970708" title="Номер телефона - аренда ямобура" rel="nofollow" itemprop="telephone">+7 (903) 797-07-08</a></br>
-<a href="tel:74957970708" title="Номер телефона - аренда спецтехники" rel="nofollow" itemprop="faxNumber">+7 (495) 797-07-08</a>
+<a href="tel:74957970708" title="Номер телефона - аренда спецтехники" rel="nofollow" itemprop="faxNumber">+7 (495) 797-07-08</a></br>
+<a href="tel:79262069799" title="Номер телефона - аренда ямобура" rel="nofollow" itemprop="telephone">+7 (926) 206-97-99</a>
